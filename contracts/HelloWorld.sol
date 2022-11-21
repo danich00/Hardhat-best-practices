b0809ee7a7202b7a1d0226924da5330266c8d686
Dani Checa <danicheca.00@gmail.com>
2022-11-21 14:20:22 +0100

    Upload project

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract HelloWorld {
+    
+    string hello = "hello";
+
+    function helloWorld() public view returns (string memory) {
+        return hello;
+    }
+}
